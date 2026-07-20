99d6208e8cd1eb5696375b7e684243719f7b9a5a
Kaswan graphics <oneframe440@gmail.com>
2026-07-21 03:13:27 +0530

    Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
-
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 70,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
